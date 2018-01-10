172a2250e2393f6ef16400af65b7002bb040d8f3
pathaj21 <patrickhajdukiewiczhome@gmail.com>
2018-01-10 14:18:05 -0500

    character class + movement

--- SchoolSim.py
+++ SchoolSim.py
@@ -1,5 +1,38 @@
-# Project - Platformer
-# Patrick Hajdukiewicz
-# Aidan Parkhurst
-# Nadil Ranatunga
-# Jake Valencia
+""" Project - Platformer
+ Patrick Hajdukiewicz
+ Aidan Parkhurst
+ Nadil Ranatunga
+ Jake Valencia"""
+
+from pygame import *
+
+
+class Character(sprite.Sprite):
+    def __init__(self, color, height, width, x, y, left, right, up):
+        pygame.sprite.Sprite.__init__(self)
+        self.color = color
+        self.height = height
+        self.width = width
+        self.x = x
+        self.y = y
+        self.image = Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.left = left
+        self.right = right
+        self.up = up
+
+    def moveRight(self):
+        self.x += 1
+        self.right = True
+
+    def moveLeft(self):
+        self.x -= 1
+        self.left = True
+
+    def moveUp(self):
+        self.y += 8
+        self.up = True
+
+    def keyPressing(self):
+        key.get_focused()
